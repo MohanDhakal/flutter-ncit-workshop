88c450adba20921fa784177093992c9324913e3e
MohanDhakal <mohandkl512@gmail.com>
2020-02-08 09:21:17 +0545

    some changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:start_app/practice/demo.dart';
 import 'package:start_app/practice/home.dart';
-import 'package:start_app/random/homescreen.dart';
-import 'package:start_app/practice/write_post.dart';
 
 void main() => runApp(
   MaterialApp(
